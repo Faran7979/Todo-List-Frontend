5faf27aec33dfa272c1a38f2f338d1cd2426ea48
Faran7979 <fari79amiri@gmail.com>
2024-07-07 11:49:43 +0330

    designing App.js

--- todo-list-frontend/src/App.js
+++ todo-list-frontend/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import CreateAccount from './components/CreateAccount';
+import Login from './components/Login';
+import Dashboard from './components/Dashboard';
+
+const App = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/create-account" component={CreateAccount} />
+        <Route path="/login">
+          <Login setLoggedIn={setLoggedIn} />
+        </Route>
+        <Route path="/dashboard">
+          {loggedIn ? <Dashboard /> : <Redirect to="/login" />}
+        </Route>
+        <Redirect from="/" to="/login" />
+      </Switch>
+    </Router>
   );
-}
+};
 
 export default App;
